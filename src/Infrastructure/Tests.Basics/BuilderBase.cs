62d163cee3b52b1c3949cca3d0ab548447eeb385
Mayeenuddin Bappi <mbappi@agiboo.com>
2020-10-23 16:35:10 +0200

    Added BuilderBase and Null Instance to assist writing unit tests

---
+++ src/Infrastructure/Tests.Basics/BuilderBase.cs
@@ -0,0 +1,59 @@
+﻿using Infrastructure.Basics;
+using Moq;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace Tests.Basics
+{
+    public abstract class BuilderBase<T> where T : class
+    {
+        private readonly IDictionary<string, object> _mockContainer;
+
+        protected BuilderBase()
+        {
+            _mockContainer = new Dictionary<string, object>();
+        }
+
+        public T Build()
+        {
+            return BuildInternal();
+        }
+
+        protected abstract T BuildInternal();
+
+        [NotNull]
+        public TInput Get<TInput>() where TInput : class
+        {
+            var mock = GetMock<TInput>();
+            return mock.Object;
+        }
+
+        public Mock<TInput> GetMock<TInput>() where TInput : class
+        {
+            var key = typeof(TInput).FullName;
+            _mockContainer.TryGetValue(key, out object dependency);
+
+            if (dependency == null)
+            {
+                var mock = new Mock<TInput>();
+                WithMock(mock);
+                return mock;
+            }
+
+            return (Mock<TInput>)dependency;
+        }
+
+        [NotNull]
+        public BuilderBase<T> WithMock<TDependency>([NotNull] Mock<TDependency> mock) where TDependency : class
+        {
+            Guard.AgainstNull(mock, nameof(mock));
+            var key = typeof(TDependency).FullName;
+
+            if (!_mockContainer.ContainsKey(key))
+            {
+                _mockContainer.Add(key, mock);
+            }
+            return this;
+        }
+    }
+}
---
+++ src/Infrastructure/Tests.Basics/NullInstance.cs
@@ -0,0 +1,10 @@
+﻿namespace Tests.Basics
+{
+    public static class NullInstance
+    {
+        public static T GetNullInstance<T>() where T : class
+        {
+            return default(T);
+        }
+    }
+}
