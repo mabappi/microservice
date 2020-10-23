de74d80976af9c13c7a7b523e97f422a28f44f59
Mayeenuddin Bappi <mbappi@agiboo.com>
2020-10-23 17:04:10 +0200

    Exception Handler for AspNet Web App

---
+++ src/Infrastructure/Infrastructure.Exceptions/ExceptionHandler.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace Infrastructure.Exceptions
+{
+    public static class ExceptionHandler
+    {
+        public static async Task<ActionResult<T>> TryCatch<T>([NotNull]Func<Task<T>> tryAction, [CanBeNull]ILogger logger = null)
+        {
+            try
+            {
+                return new OkObjectResult(await tryAction());
+            }
+            catch (NotFoundException notFoundException)
+            {
+                logger?.LogError(notFoundException, notFoundException.Message);
+                return new NotFoundObjectResult(notFoundException.Message);
+            }
+            catch (DomainException domainException)
+            {
+                logger?.LogError(domainException, domainException.Message);
+                return new BadRequestObjectResult(domainException.ToUserSummary());
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex, ex.Message);
+                return new BadRequestObjectResult(ex.Message);
+            }
+        }
+    }
+}
