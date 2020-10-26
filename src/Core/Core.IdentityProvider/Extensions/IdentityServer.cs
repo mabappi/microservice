3106d26632021e518f4c09c3d90b5dc99785aeef
Mayeenuddin Bappi <mbappi@agiboo.com>
2020-10-26 20:39:46 +0100

    Added Idenity Server

---
+++ src/Core/Core.IdentityProvider/Extensions/IdentityServer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Core.IdentityProvider.Extensions
+{
+    public static class IdentityServer
+    {
+        private const string DefaultConnectionString = "DefaultConnection";
+
+        public static IServiceCollection AddIdentityServer(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString(DefaultConnectionString);
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddDbContext<IdentityDbContext>(options =>
+                options.UseSqlServer(connectionString,
+                    sqlServerOptionsAction: sqlOptions => {
+                        sqlOptions.MigrationsAssembly(typeof(Startup).GetTypeInfo().Assembly.GetName().Name);
+                        sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30),
+                            errorNumbersToAdd: null);
+                    }));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddOpenIdConnect("Azure AD / Microsoft", "Azure AD / Microsoft", options => // Microsoft common
+                {
+                    options.ClientId = configuration["ClientId"];
+                    options.ClientSecret = configuration["ClientSecret"];
+                    options.SignInScheme = "IdentityAsAService";
+                    options.RemoteAuthenticationTimeout = TimeSpan.FromSeconds(30);
+                    options.Authority = "https://login.microsoftonline.com/common/v2.0/";
+                    options.ResponseType = "code";
+                    options.Scope.Add("profile");
+                    options.Scope.Add("email");
+                    options.TokenValidationParameters = new TokenValidationParameters {
+                        ValidateIssuer = false,
+                        NameClaimType = "email",
+                    };
+                    options.CallbackPath = "/signin-microsoft";
+                    options.Prompt = "login"; // login, consent
+                });
+
+            services.AddIdentityServer(x => {
+                    x.Authentication.CookieLifetime = TimeSpan.FromMinutes(15);
+                })
+                .AddDeveloperSigningCredential()
+                .AddAspNetIdentity<IdentityUser>()
+                .AddConfigurationStore(options => {
+                    options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString,
+                        sqlServerOptionsAction: sqlOptions => {
+                            sqlOptions.MigrationsAssembly(migrationsAssembly);
+                            sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30),
+                                errorNumbersToAdd: null);
+                        });
+                })
+                .AddOperationalStore(options => {
+                    options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString,
+                        sqlServerOptionsAction: sqlOptions => {
+                            sqlOptions.MigrationsAssembly(migrationsAssembly);
+                            sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30),
+                                errorNumbersToAdd: null);
+                        });
+                });
+            return services;
+
+        }
+    }
+}
--- src/Core/Core.IdentityProvider/Startup.cs
+++ src/Core/Core.IdentityProvider/Startup.cs
@@ -1,4 +1,9 @@
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +15,8 @@ namespace Core.IdentityProvider
 {
     public class Startup
     {
+        private const string CorsPolicyName = "CorsPolicy";
+
         private IConfiguration Configuration { get; }
 
         public Startup(IConfiguration configuration)
@@ -22,10 +29,20 @@ namespace Core.IdentityProvider
         public void ConfigureServices(IServiceCollection services)
         {
             services
-                .Configure<ForwardedHeadersOptions>(options =>
-                {
+                .Configure<ForwardedHeadersOptions>(options => {
                     options.ForwardedHeaders = ForwardedHeaders.All;
                 });
+            services.AddDataProtection()
+                .PersistKeysToFileSystem(new DirectoryInfo(@"/dataprotection"))
+                .SetApplicationName("ApplicationCookie");
+            services.Configure<CookiePolicyOptions>(options => {
+                options.MinimumSameSitePolicy = SameSiteMode.Lax;
+            });
+            services
+                .Configure<CookieAuthenticationOptions>(SetCookieOptions)
+                .ConfigureApplicationCookie(SetCookieOptions);
+            services.AddCors(x => x.AddPolicy(CorsPolicyName, builder => builder.SetIsOriginAllowed(host => true).AllowAnyHeader().AllowAnyMethod().AllowCredentials()));
+            services.AddIdentityServer(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,8 +54,11 @@ namespace Core.IdentityProvider
             }
 
             app
-                .UseForwardedHeaders();
-            app.UseRouting();
+                .UseForwardedHeaders()
+                .UseCors(CorsPolicyName)
+                .UseIdentityServer()
+                .UseAuthorization()
+                .UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
@@ -48,5 +68,18 @@ namespace Core.IdentityProvider
                 });
             });
         }
+
+        private void SetCookieOptions(CookieAuthenticationOptions options)
+        {
+            options.Cookie.Path = "/";
+            options.SlidingExpiration = true;
+            options.ExpireTimeSpan = TimeSpan.FromMinutes(15);
+            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            if (!string.IsNullOrEmpty(Configuration["DomainName"]))
+            {
+                options.Cookie.Domain = Configuration["DomainName"];
+            }
+        }
+
     }
 }
